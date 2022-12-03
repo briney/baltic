3a687ef8667a2b87e10ac4863ca8b9881a05cc94
Bryan Briney <bryanbriney@gmail.com>
2022-12-03 01:22:49 -0800

    begin refactor

---
+++ baltic/io.py
@@ -0,0 +1,294 @@
+from io import BytesIO as csio
+import json
+import re
+import requests
+from typing import Union, IO
+
+from .tree import Tree, make_tree, make_tree_json
+
+
+def load_newick(
+    tree_path: Union[str, IO],
+    tip_regex: str = "\|([0-9]+\-[0-9]+\-[0-9]+)",
+    date_fmt: str = "%Y-%m-%d",
+    variable_date: bool = True,
+    absolute_time: bool = False,
+    verbose: bool = False,
+    sort_branches: bool = True,
+) -> Tree:
+    """
+    Load newick file
+    """
+    ll = None
+    if isinstance(tree_path, str):
+        handle = open(tree_path, "r")
+    else:
+        handle = tree_path
+
+    for line in handle:
+        l = line.strip("\n")
+        if "(" in l:
+            tree_string_start = l.index("(")
+            ll = make_tree(l[tree_string_start:], verbose=verbose)
+            if verbose:
+                print("Identified tree string")
+
+    assert ll, "Regular expression failed to find tree string"
+    ll.traverse_tree(verbose=verbose)  ## traverse tree
+    if sort_branches:
+        ll.sort_branches()  ## traverses tree, sorts branches, draws tree
+
+    if absolute_time:
+        tip_dates = []
+        tip_names = []
+        for k in ll.get_external():
+            n = k.name
+            tip_names.append(n)
+            cerberus = re.search(tip_regex, n)
+            if cerberus is not None:
+                tip_dates.append(
+                    decimal_date(
+                        cerberus.group(1), fmt=date_fmt, variable=variable_date
+                    )
+                )
+        assert len(tip_dates) > 0, (
+            "Regular expression failed to find tip dates in tip names, review regex pattern or set absolute_time option to False.\nFirst tip name encountered: %s\nDate regex set to: %s\nExpected date format: %s"
+            % (tip_names[0], tip_regex, date_fmt)
+        )
+        highest_tip = max(tip_dates)
+        ll.set_absolute_time(highest_tip)
+    if isinstance(tree_path, str):
+        handle.close()
+    return ll
+
+
+def load_nexus(
+    tree_path: Union[str, IO],
+    tip_regex: str = "\|([0-9]+\-[0-9]+\-[0-9]+)",
+    date_fmt: str = "%Y-%m-%d",
+    tree_string_regex: str = "tree [A-Za-z\_]+([0-9]+)",
+    variable_date: bool = True,
+    absolute_time: bool = True,
+    verbose: bool = False,
+    sort_branches: bool = True,
+) -> Tree:
+    """
+    Load nexus file
+    """
+    tipFlag = False
+    tips = {}
+    tipNum = 0
+    ll = None
+    if isinstance(tree_path, str):
+        handle = open(tree_path, "r")
+    else:
+        handle = tree_path
+
+    for line in handle:
+        l = line.strip("\n")
+
+        cerberus = re.search("dimensions ntax=([0-9]+);", l.lower())
+        if cerberus is not None:
+            tipNum = int(cerberus.group(1))
+            if verbose == True:
+                print("File should contain %d taxa" % (tipNum))
+
+        cerberus = re.search(tree_string_regex, l.lower())
+        if cerberus is not None:
+            tree_string_start = l.index("(")
+            ll = make_tree(
+                l[tree_string_start:]
+            )  ## send tree string to make_tree function
+            if verbose == True:
+                print("Identified tree string")
+
+        if tipFlag == True:
+            cerberus = re.search("([0-9]+) ([A-Za-z\-\_\/\.'0-9 \|?]+)", l)
+            if cerberus is not None:
+                tips[cerberus.group(1)] = cerberus.group(2).strip('"').strip("'")
+                if verbose == True:
+                    print(
+                        "Identified tip translation %s: %s"
+                        % (cerberus.group(1), tips[cerberus.group(1)])
+                    )
+            elif ";" not in l:
+                print("tip not captured by regex:", l.replace("\t", ""))
+
+        if "translate" in l.lower():
+            tipFlag = True
+        if ";" in l:
+            tipFlag = False
+
+    assert ll, "Regular expression failed to find tree string"
+    ll.traverse_tree()  ## traverse tree
+    if sort_branches:
+        ll.sort_branches()  ## traverses tree, sorts branches, draws tree
+    if len(tips) > 0:
+        ll.renameTips(tips)  ## renames tips from numbers to actual names
+        ll.tipMap = tips
+    if absolute_time:
+        tip_dates = []
+        tip_names = []
+        for k in ll.get_external():
+            tip_names.append(k.name)
+            cerberus = re.search(tip_regex, k.name)
+            if cerberus is not None:
+                tip_dates.append(
+                    decimal_date(
+                        cerberus.group(1), fmt=date_fmt, variable=variable_date
+                    )
+                )
+
+        assert len(tip_dates) > 0, (
+            "Regular expression failed to find tip dates in tip names, review regex pattern or set absolute_time option to False.\nFirst tip name encountered: %s\nDate regex set to: %s\nExpected date format: %s"
+            % (tip_names[0], tip_regex, date_fmt)
+        )
+        highest_tip = max(tip_dates)
+        ll.set_absolute_time(highest_tip)
+
+    if isinstance(tree_path, str):
+        handle.close()
+    return ll
+
+
+def load_json(
+    json_object: Union[str, IO],
+    json_translation: dict = {"name": "name", "absolute_time": "num_date"},
+    verbose: bool = False,
+    sort: bool = True,
+    stats: bool = True,
+):
+    """
+    Load a nextstrain JSON by providing either the path to JSON or a file handle.
+    json_translation is a dictionary that translates JSON attributes to baltic branch attributes (e.g. 'absolute_time' is called 'num_date' in nextstrain JSONs).
+    Note that to avoid conflicts in setting node heights you can either define the absolute time of each node or branch lengths (e.g. if you want a substitution tree).
+    """
+    assert "name" in json_translation and (
+        "absolute_time" in json_translation
+        or "length" in json_translation
+        or "height" in json_translation
+    ), (
+        "JSON translation dictionary missing entries: %s"
+        % (
+            ", ".join(
+                [
+                    entry
+                    for entry in ["name", "height", "absolute_time", "length"]
+                    if entry not in json_translation
+                ]
+            )
+        )
+    )
+    if verbose:
+        print("Reading JSON")
+
+    if isinstance(
+        json_object, str
+    ):  ## string provided - either nextstrain URL or local path
+        if "nextstrain.org" in json_object:  ## nextsrain.org in URL - request it
+            if verbose:
+                print("Assume URL provided, loading JSON from nextstrain.org")
+            auspice_json = json.load(csio(requests.get(json_object).content))
+        else:  ## not nextstrain.org URL - assume local path to auspice v2 json
+            if verbose:
+                print("Loading JSON from local path")
+            with open(json_object) as json_data:
+                auspice_json = json.load(json_data)
+    else:  ## not string, assume auspice v2 json object given
+        if verbose:
+            print("Loading JSON from object given")
+        auspice_json = json_object
+
+    json_meta = auspice_json["meta"]
+    json_tree = auspice_json["tree"]
+    ll = make_tree_json(json_tree, json_translation, verbose=verbose)
+
+    assert (
+        "absolute_time" in json_translation
+        and ("length" not in json_translation or "height" not in json_translation)
+    ) or (
+        "absolute_time" not in json_translation
+        and ("length" in json_translation or "height" in json_translation)
+    ), "Cannot use both absolute time and branch length, include only one in json_translation dictionary."
+
+    if verbose:
+        print("Setting baltic traits from JSON")
+    for k in ll.Objects:  ## make node attributes easier to access
+        for key in k.traits["node_attrs"]:
+            if isinstance(k.traits["node_attrs"][key], dict):
+                if "value" in k.traits["node_attrs"][key]:
+                    k.traits[key] = k.traits["node_attrs"][key]["value"]
+                if "confidence" in k.traits["node_attrs"][key]:
+                    k.traits["%s_confidence" % (key)] = k.traits["node_attrs"][key][
+                        "confidence"
+                    ]
+            elif key == "div":
+                k.traits["divergence"] = k.traits["node_attrs"][key]
+
+    for attr in json_translation:  ## iterate through attributes in json_translation
+        for k in ll.Objects:  ## for every branch
+            if isinstance(json_translation[attr], str):
+                if json_translation[attr] in k.traits:
+                    setattr(
+                        k, attr, k.traits[json_translation[attr]]
+                    )  ## set attribute value for branch
+                elif (
+                    "node_attrs" in k.traits
+                    and json_translation[attr] in k.traits["node_attrs"]
+                ):
+                    setattr(k, attr, k.traits["node_attrs"][json_translation[attr]])
+                elif (
+                    "branch_attrs" in k.traits
+                    and json_translation[attr] in k.traits["branch_attrs"]
+                ):
+                    setattr(k, attr, k.traits["branch_attrs"][json_translation[attr]])
+                else:
+                    raise KeyError(
+                        "String attribute %s not found in JSON"
+                        % (json_translation[attr])
+                    )
+            elif callable(json_translation[attr]):
+                setattr(
+                    k, attr, json_translation[attr](k)
+                )  ## set attribute value with a function for branch
+            else:
+                raise AttributeError(
+                    "Attribute %s neither string nor callable"
+                    % (json_translation[attr])
+                )
+
+    for branch_unit in [
+        "height",
+        "absolute_time",
+    ]:  ## iterate between divergence and absolute time
+        if branch_unit in json_translation:  ## it's available in tree
+            for k in ll.Objects:  ## iterate over all branches
+                cur_branch = getattr(k, branch_unit)  ## get parameter for this branch
+                par_branch = getattr(
+                    k.parent, branch_unit
+                )  ## get parameter for parental branch
+                k.length = (
+                    cur_branch - par_branch if cur_branch and par_branch else 0.0
+                )  ## difference between current and parent is branch length (or, if parent unavailabel it's 0)
+
+    if verbose:
+        print("Traversing and drawing tree")
+
+    ll.traverse_tree(verbose=verbose)
+    ll.drawTree()
+    if stats:
+        ll.treeStats()  ## initial traversal, checks for stats
+    if sort:
+        ll.sort_branches()  ## traverses tree, sorts branches, draws tree
+
+    cmap = {}
+    for colouring in json_meta["colorings"]:
+        if colouring["type"] == "categorical" and "scale" in colouring:
+            cmap[colouring["key"]] = {}
+            for entry in colouring["scale"]:
+                key, value = entry
+                cmap[colouring["key"]][key] = value
+    setattr(ll, "cmap", cmap)
+
+    return ll, json_meta
+
---
+++ baltic/models.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# filename: models.py
+#
+# Inspiration: https://github.com/evogytis/baltic
+# License: GNU General Public License v3.0 (https://opensource.org/licenses/GPL-3.0)
+#
+
+
+from typing import Union
+
+
+class BaseTreeObject:
+    """
+    docstring for BaseTreeObject
+    """
+
+    def __init__(self):
+        self._name = None
+        self._branch_type = None
+        self._length = None
+        self._width = None
+        self._height = None
+        self._absolute_time = None
+        self._parent = None
+        self._traits = {}
+        self._index = None
+        self._x = None
+        self._y = None
+
+    @property
+    def name(self) -> Union[str, None]:
+        """
+        object name
+        """
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+    @property
+    def branch_type(self) -> Union[str, None]:
+        """
+        Options are 'node' or 'leaf'
+        """
+        return self._branch_type
+
+    @branch_type.setter
+    def branch_type(self, branch_type: Union["node", "leaf"]):
+        self._branch_type = branch_type
+
+    @property
+    def length(self) -> float:
+        """
+        branch length
+        """
+        return self._length
+
+    @length.setter
+    def length(self, length: float):
+        self._length = length
+
+    @property
+    def width(self) -> Union[float, int, None]:
+        """
+        branch width
+        """
+        return self._width
+
+    @width.setter
+    def width(self, width: float):
+        self._width = width
+
+    @property
+    def height(self) -> Union[float, int, None]:
+        """
+        height, set by traversing the tree, which adds up branch lengths along the way
+        """
+        return self._height
+
+    @height.setter
+    def height(self, height: float):
+        self._height = height
+
+    @property
+    def absolute_time(self):
+        """
+        branch end point in absolute time, once calibrations are done
+        """
+        return self._absolute_time
+
+    @absolute_time.setter
+    def absolute_time(self, absolute_time):
+        self._absolute_time = absolute_time
+
+    @property
+    def parent(self):
+        """
+        reference to parent object
+        """
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent):
+        self._parent = parent
+
+    @property
+    def traits(self) -> dict:
+        """
+        dictionary that will contain annotations from the tree string, e.g. {'posterior':1.0}
+        """
+        return self._traits
+
+    @traits.setter
+    def traits(self, traits: dict):
+        self._traits = traits
+
+    @property
+    def index(self) -> Union[float, int, None]:
+        """
+        index of the character designating this object in the tree string 
+        it's a unique identifier for every object in the tree
+        """
+        return self._index
+
+    @index.setter
+    def index(self, index: int):
+        self._index = index
+
+    @property
+    def x(self) -> Union[float, int, None]:
+        """
+        X coordinate of the object, once draw_tree() is called
+        """
+        return self._x
+
+    @x.setter
+    def x(self, x: float):
+        self._x = x
+
+    @property
+    def y(self) -> Union[float, int, None]:
+        """
+        Y coordinate of the object, once draw_tree() is called
+        """
+        return self._y
+
+    @y.setter
+    def y(self, y: float):
+        self._y = y
+
+    def is_leaflike(self):
+        return False
+
+    def is_leaf(self):
+        return False
+
+    def is_node(self):
+        return False
+
+
+class Node(BaseTreeObject):
+    """
+    docstring for Node
+    """
+
+    def __init__(self):
+        super(Node, self).__init__()
+        self.branch_type = "node"
+        self.length = 0.0  ## branch length, recovered from string
+        self.children = []  ## a list of descendent branches of this node
+        self.child_height = None  ## the youngest descendant tip of this node
+        ## contains references to all tips of this node
+        self.leaves = set()  ## is a set of tips that are descended from it
+
+    def is_node(self):
+        return True
+
+
+class Leaf(BaseTreeObject):
+    """
+    docstring for Leaf
+    """
+
+    def __init__(self):
+        super(Leaf, self).__init__()
+        self.branchType = "leaf"
+
+    def is_leaflike(self):
+        return False
+
+    def is_leaf(self):
+        return True
+
+    def is_node(self):
+        return False
+
+
+class Clade(BaseTreeObject):
+    """
+    docstring for Clade
+    """
+
+    def __init__(self, name: str):
+        super(Clade, self).__init__()
+        self.name = name  ## the pretend tip name for the clade
+        self.branch_type = "leaf"  ## clade class poses as a leaf
+        self.subtree = None
+        self.leaves = None
+        self.length = 0.0
+        self.width = 1
+        ## refers to the height of the highest tip in the collapsed clade
+        self.last_height = None
+        ## refers to the absolute time of the highest tip in the collapsed clade
+        self.last_absolute_time = None
+
+    def is_leaflike(self):
+        return True
+
+
+class Reticulation(BaseTreeObject):
+    """
+    reticulation class (recombination, conversion, reassortment)
+    """
+
+    def __init__(self, name):
+        super(Reticulation, self).__init__()
+        self.name = name
+        self.branch_type = "leaf"
+        self.length = 0.0
+        self.height = 0.0
+        self.width = 0.5
+        self.target = None
+
+    def is_leaflike(self):
+        return True
+
